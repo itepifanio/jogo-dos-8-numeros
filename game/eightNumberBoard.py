d192059cfcc89f571a255c6f36f4c8e55a5712aa
Ítalo Epifânio <italoepifaniols@gmail.com>
2021-07-03 18:49:46 -0300

    Moves elements throught the board

---
+++ game/eightNumberBoard.py
@@ -0,0 +1,73 @@
+from .board import Board
+from random import choice
+
+class EightNumberBoard:
+    def __init__(self):
+        self.size = 3
+        self.board = Board(self.size)
+        queue = list(range(0,9))
+        
+        for i in range(self.size):
+            for j in range(self.size):
+                value = choice(queue)
+                queue.remove(value)
+                
+                self.board.update(i,j, value if value != 0 else None)
+
+    def getBoard(self):
+        return self.board
+
+    def canMoveLeft(self, i, j):
+        try:
+            return self.board.matrix[i][j-1] == None
+        except:
+            return False
+
+    def left(self, i, j):
+        if self.canMoveLeft(i, j):
+            self.board.matrix[i][j-1] = self.board.matrix[i][j]
+            self.board.matrix[i][j] = None
+        else:
+            raise Exception("Cant go left")
+
+    def canMoveRight(self, i, j):
+        try:
+            return self.board.matrix[i][j+1] == None
+        except:
+            return False
+
+    def right(self, i, j):
+        if self.canMoveRight(i, j):
+            self.board.matrix[i][j+1] = self.board.matrix[i][j]
+            self.board.matrix[i][j] = None
+        else:
+            raise Exception("Cant go right")
+
+    def canMoveBottom(self, i, j):
+        try:
+            return self.board.matrix[i+1][j] == None
+        except:
+            return False
+
+    def bottom(self, i, j):
+        if self.canMoveBottom(i, j):
+            self.board.matrix[i+1][j] = self.board.matrix[i][j]
+            self.board.matrix[i][j] = None
+        else:
+            raise Exception("Cant go bottom")
+
+    def canMoveTop(self, i, j):
+        try:
+            return self.board.matrix[i-1][j] == None
+        except:
+            False
+
+    def top(self, i, j):
+        if self.canMoveTop(i, j):
+            self.board.matrix[i-1][j] = self.board.matrix[i][j]
+            self.board.matrix[i][j] = None
+        else:
+            raise Exception("Cant go bottom")
+    
+    def __str__(self):
+        return self.board.__str__()
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from game.eightNumber import EightNumber
+from game.eightNumberBoard import EightNumberBoard
 
 def main():
-    eightNumber = EightNumber()
+    eightNumber = EightNumberBoard()
     print(eightNumber)
+    
+    board = eightNumber.getBoard().matrix
+    for i in range(len(board)):
+        for j in range(len(board)):
+            if(eightNumber.canMoveBottom(i, j)):
+                eightNumber.bottom(i, j)
+                print(eightNumber)
+
+
 
 if __name__ == "__main__":
     main()
