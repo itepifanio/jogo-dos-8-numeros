4d5197b252cdf41b90ce750db23e9b5dc1303c99
Ítalo Epifânio <italoepifaniols@gmail.com>
2021-07-20 21:50:04 -0300

    Another heuristic

--- lessErrors/main.py
+++ lessErrors/main.py
@@ -1,75 +1,90 @@
+from typing import final
 from game.eightNumberBoard import EightNumberBoard
 from heapq import heappush, heappop
 import time
+import itertools
+import copy
+import time
+
+def printNode(node):
+    print(node[0][0],node[0][1],node[0][2])
+    print(node[1][0],node[1][1],node[1][2])
+    print(node[2][0],node[2][1],node[2][2])
+    global nodeNumber
+    print('Node:', nodeNumber)
+    print('------')
+    nodeNumber += 1
 
 def toZero(a):
     return 0 if a == None else a
 
-def manhattan(node, finalNode):
+def checkFinal(node, finalNode):
+    if node == finalNode:
+        printNode(node)
+        return True
+    if node not in visitedList:
+        printNode(node)
+        nodeList.append(node)
+        visitedList.append(node)
+    return False
+
+def heuristic(node, finalNode):
     errors = 0
+
     for i in range(len(node)):
         for j in range(len(node)):
-            if node[i][j] == finalNode[i][j]:
+            if node[i][j] != finalNode[i][j]:
                 errors += 1
+
     return errors
-    result = 0
-
-    for i in range(len(a)):
-        result += sum(abs(val1-val2) for val1, val2 in zip(a[i],b[i]))
-
-    return result
 
 if __name__ == '__main__':
     board = EightNumberBoard()
     
-    finalNode = [[1,2,3], [8,0,4], [7,6,5]]
+    startNode = board.getBoard()
+    print(startNode)
+    finalNode = [[1, 2, 3], [8, 0, 4], [7, 6, 5]]
     
-    priorityQueue = []
-    
-    heappush(priorityQueue, (0, board.getBoard()))
-
-    checkFinal = lambda x: x == finalNode
+    found = False
+    nodeNumber = 0
+    visitedList = []
+    nodeList = []
+    nodeList.append(startNode)
+    visitedList.append(startNode)
 
     t0 = time.time()
 
     found = False
 
-    while (not found and len(priorityQueue) != 0):
-        currentNode = heappop(priorityQueue)[1]
+    while not found:
+        fList = []
+        for node in nodeList:
+            h = heuristic(node, finalNode)
+            heappush(fList, (h, node))
+
+        currentNode = nodeList.pop(
+            nodeList.index(
+                heappop(fList)[1]
+            )
+        )
 
         blankIndex = [[i, n.index(0)] for i, n in enumerate(currentNode) if 0 in n][0]
-        
-        index = 0
 
         if board.canMoveTop(blankIndex):
             topNode = board.top(currentNode, blankIndex[0], blankIndex[1])
-            found = checkFinal(topNode)
-            heappush(
-                priorityQueue, (manhattan(topNode, finalNode), topNode)
-            )
+            found = checkFinal(topNode, finalNode)
 
         if board.canMoveLeft(blankIndex) and found == False:
             leftNode = board.left(currentNode, blankIndex[0], blankIndex[1])
-            found = checkFinal(leftNode)
-            heappush(
-                priorityQueue, (manhattan(leftNode, finalNode), leftNode)
-            )
+            found = checkFinal(leftNode, finalNode)
 
         if board.canMoveRight(blankIndex) and found == False:
             rightNode = board.right(currentNode, blankIndex[0], blankIndex[1])
-            found = checkFinal(rightNode)
-            heappush(
-                priorityQueue, (manhattan(rightNode, finalNode), rightNode)
-            )
+            found = checkFinal(rightNode, finalNode)
 
         if board.canMoveBottom(blankIndex) and found == False:
             bottomNode = board.bottom(currentNode, blankIndex[0], blankIndex[1])
-            found = checkFinal(bottomNode)
-            heappush(
-                priorityQueue, (manhattan(bottomNode, finalNode), bottomNode)
-            )
-
-            print("Queue {}".format(len(priorityQueue)))
+            found = checkFinal(bottomNode, finalNode)
 
     t1 = time.time()
     print('Time:', t1-t0)
--- manhattan/main.py
+++ manhattan/main.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     board = EightNumberBoard()
     
     startNode = board.getBoard()
-    finalNode = [[0,1,2], [3,4,5], [6,7,8]]
+    finalNode = [[1, 2, 3], [8, 0, 4], [7, 6, 5]]
     
     found = False
     nodeNumber = 0
